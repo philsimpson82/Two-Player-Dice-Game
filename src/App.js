3bf990bac32793909a61790ff9854b5b06046daa
Phil Simpson <phil_simpson@att.net>
2023-02-23 17:22:18 -0600

    minor changes 2_23_23

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import { useState } from 'react';
 import { Container, Button, Row, Col } from 'reactstrap';
-// import die_face_1 from './assets/img/die_face_1_T.png';
-// import die_face_2 from './assets/img/die_face_2_T.png';
-// import die_face_3 from './assets/img/die_face_3_T.png';
-// import die_face_4 from './assets/img/die_face_4_T.png';
-// import die_face_5 from './assets/img/die_face_5_T.png';
-// import die_face_6 from './assets/img/die_face_6_T.png';
-// import die_face_7 from './assets/img/die_face_7_T.png';
+import './App.css';
 
 function App() {
-  const [newGame, setNewGame] = useState(true);
+  const [startGame, setStartGame] = useState(true);
   const [currentPlayer, setCurrentPlayer] = useState(1);
   let [currentScore, setCurrentScore] = useState(0);
-  const [dice, setDice] = useState(7);
   const [playerOneScore, setPlayerOneScore] = useState(0);
   const [playerTwoScore, setPlayerTwoScore] = useState(0);
-  let [dieFace, setDieFace] = useState(7);
+  const [dieFace, setDieFace] = useState(7);
+
+  function newGame () {
+    setStartGame(false);
+    setCurrentPlayer(1);
+    setCurrentScore(0);
+    setPlayerOneScore(0);
+    setPlayerTwoScore(0);
+    setDieFace(7);
+  }
 
   function rollCalculator () {
     let rollValue = Math.floor((Math.random() * 6) + 1);
-    setDice(rollValue);
     setDieFace(rollValue);
     setCurrentScore(currentScore += rollValue);
     console.log(dieFace);
@@ -35,10 +36,17 @@ function App() {
       setPlayerOneScore(playerOneScore + currentScore);
       setCurrentPlayer(2);
       setCurrentScore(0);
+      setDieFace(7);
     } else {
       setPlayerTwoScore(playerTwoScore + currentScore);
       setCurrentPlayer(1);
       setCurrentScore(0);
+      setDieFace(7);
+    }
+    if (playerOneScore >= 100) {
+      //setGameWinner(1);
+    } else {
+      //setGameWinner(2);
     }
   }
 
@@ -47,22 +55,25 @@ function App() {
     setCurrentScore(0);
     setPlayerOneScore(0);
     setPlayerTwoScore(0);
-    setDieFace(`die_face_7`);
+    setDieFace(7);
+    setStartGame(true);
   }
 
   return (
-    <Container>
-      <Row className='row-content'>
-        <Col className='text-start my-3' md='6'>Player 1 Score {playerOneScore}</Col>
-        <Col className='text-end my-3' md='6'>Player 2 Score {playerTwoScore}</Col>
+    <Container className='feltTable'>
+      <Row className='row-content scoreCard'>
+        <Col className='text-start my-3' md='6'>Player 1 Score: {playerOneScore}</Col>
+        <Col className='text-end my-3' md='6'>Player 2 Score: {playerTwoScore}</Col>
       </Row>
-      <Row className='row-content d-flex justify-content-center'>
-        <h3 className='text-center'>Welcome to Dice Roll!</h3>
+      <Row className='row-content m-3 d-flex justify-content-center'>
+        <h2 className='text-center'>Welcome to Dice Roll!</h2>
         {`Current Player is Player ${currentPlayer}`}
       </Row>
       <Row className='row-content d-flex justify-content-center'>
-        <img className='dice img-thumbnail' src={`../img/die_face_${dieFace}_T.png`} alt='die face' />
-        {currentScore}
+        <img className='dice m-5' src={`../img/die_face_${dieFace}_T.png`} alt='die face' />
+      </Row>
+      <Row className='row-content d-flex justify-content-center'>
+        <h4 className='text-center'>Current Score Count: {currentScore}</h4>
       </Row>
       <Row className='row-content'>
         <Col className='d-flex justify-content-start my-3'>
